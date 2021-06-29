e955ead85afcafb6087bb5d1f6390df1bce2be67
milkuri-prashanth <86643497+milkuri-prashanth@users.noreply.github.com>
2021-06-29 13:47:14 +0530

    Update settings.py

--- jobproject/settings.py
+++ jobproject/settings.py
@@ -78,10 +78,8 @@ WSGI_APPLICATION = 'jobproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jobdb',
-        'USER': 'root',
-        'PASSWORD': 'rootpassword'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
